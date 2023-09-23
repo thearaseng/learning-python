f4c3e896a7bebc97b149b76b3560be64a3f969fd
Theara Seng <me@thearaseng.com>
2023-09-23 08:01:36 -0500

    add 005.for-loop.py

---
+++ 005.for-loop.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+colors = ['blue', 'green', 'red']
+
+for color in colors:
+  print(color)
+
+dic = [('age', 20), ('name', 'Kevin')]
+for key, value in dic:
+  print(f"key = {key}, value = {value}")
